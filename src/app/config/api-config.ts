b8e244f94a36aefb818ae51383797ad05f31c9cb
Gabriel <gabriellagrota23@gmail.com>
2023-06-13 16:55:24 -0300

    build: token de estatico de produção atualizado

--- src/app/config/api-config.ts
+++ src/app/config/api-config.ts
@@ -3,7 +3,7 @@ export const API_CONFIG = {
     baseUrl: isDevMode() ? 'http://localhost:9090/api/v1' : 'https://cinergia-backend.herokuapp.com/api/v1',
     devToken: isDevMode() 
     ? 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkxMSIsImV4cCI6MTY4NzI4Mzg2MH0.1cgjT5qQsS0ezMNsdD6vF9J9eJbUUoOPqxv3270bzmKKH7dKVuvyySUZC79JD3nSRnfZwvIktg4ttbt_z0T9KQ'
-    : 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkxMSIsImV4cCI6MTY4Njc3MDIwOX0.48VcPydrAHMKMG_wpOieG7NTgDfx1WvC72IapuTcsjuZMoi_aw36WHSxT6nm8bZsPwEa9L_DTY7ijq357G3WUQ'
+    : '	Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkxMSIsImV4cCI6MTY4NzI5MDg5Mn0.K2vtfDIEawgCVOjlQCIs2XTkQIt8h-jBqsXyL-awCqKk-evIrQFyEnCG76N1K7_KLeg0YHTQ0KZSZUKrIJTPlg'
 }
 
 // import { isDevMode } from '@angular/core';
