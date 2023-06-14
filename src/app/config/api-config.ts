afc3d4febae92b2f850fad4b6e1bc3ccbfd91b83
Gabriel <gabriellagrota23@gmail.com>
2023-06-14 11:40:29 -0300

    feat: Ajuste de hibernate para persistir imagens em banco de dados em produção realizado

--- src/app/config/api-config.ts
+++ src/app/config/api-config.ts
@@ -2,7 +2,7 @@ import { isDevMode } from '@angular/core';
 export const API_CONFIG = {
     baseUrl: isDevMode() ? 'http://localhost:9090/api/v1' : 'https://cinergia-backend.herokuapp.com/api/v1',
     devToken: isDevMode() 
-    ? 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkxMSIsImV4cCI6MTY4NzI4Mzg2MH0.1cgjT5qQsS0ezMNsdD6vF9J9eJbUUoOPqxv3270bzmKKH7dKVuvyySUZC79JD3nSRnfZwvIktg4ttbt_z0T9KQ'
+    ? '	Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkxMSIsImV4cCI6MTY4NzM1ODExMH0.lVYDtr_AjT11Ktt2O_pDVX32VsBLP5Co94uHKy82mIZ-QzjAQY8uZKC_PJhBEBO-LEd3umUdorTf5nw8Ec8G8Q'
     : '	Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkxMSIsImV4cCI6MTY4NzI5MTExNn0.Zh_jfjNmi3mnYV26bqc5TH0_VlKc9bgRSqQmEZAsOeBou8jQRt275vF3WtJ7WG2wG3VGEFHmR8FZybol7dI_fA'
 }
 
